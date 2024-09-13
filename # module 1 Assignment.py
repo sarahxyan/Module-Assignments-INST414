47f2b05c004cbceff43a15db3f156df36cbd98a3
Sarah Yan <syan9219@terpmail.umd.edu>
2024-09-12 23:49:40 -0400

    Create # module 1 Assignment.py
    access Chicago crime data API

---
+++ # module 1 Assignment.py
@@ -0,0 +1,9 @@
+# module 1 assignment
+# Script for collecting domestic violence data from the City of Chicago's public data portal
+
+import requests
+import pandas as pd
+
+# API endpoint for Chicago crime data
+url = "https://data.cityofchicago.org/resource/ijzp-q8t2.json"
+
