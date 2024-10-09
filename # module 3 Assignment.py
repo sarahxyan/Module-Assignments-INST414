10e0a511c30a0a29709e50b8ad1f64227afd8cff
Sarah Yan <Sarbear9219@gmail.com>
2024-10-09 16:33:25 -0400

    Update # module 3 Assignment.py
    load, process and aggregate data

--- # module 3 Assignment.py
+++ # module 3 Assignment.py
@@ -6,7 +6,6 @@ import requests
 import matplotlib.pyplot as plt
 import networkx as nx
 
-
 url = "https://data.cityofchicago.org/resource/ijzp-q8t2.json"
 
 params = {
@@ -17,3 +16,9 @@ params = {
 response = requests.get(url, params=params)
 data = response.json()
 dv_data = pd.DataFrame(data)
+
+dv_data['community_area'] = pd.to_numeric(dv_data['community_area'], errors='coerce')
+dv_data.dropna(subset=['community_area'], inplace=True)
+socioeconomic_data = pd.read_csv('socioeconomic_data.csv')
+socioeconomic_data['community_area'] = pd.to_numeric(socioeconomic_data['community_area'], errors='coerce')
+incident_counts = dv_data.groupby('community_area').size().reset_index(name='incident_count')
