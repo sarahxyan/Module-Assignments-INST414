945495351c8616ee48463b5b97a706a81bfaabb3
Sarah Yan <Sarbear9219@gmail.com>
2024-10-09 16:16:01 -0400

    Update # module 3 Assignment.py
    load and merge data set

--- # module 3 Assignment.py
+++ # module 3 Assignment.py
@@ -4,4 +4,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+dv_data = pd.read_csv('community_area_names.csv')
+socioeconomic_data = pd.read_csv('socioeconomic_data.csv')
 
+dv_data['community_area'] = dv_data['community_area'].astype(str)
+socioeconomic_data['community_area'] = socioeconomic_data['community_area'].astype(str)
+
+merged_df = dv_data.merge(socioeconomic_data, how='left', on='community_area')
