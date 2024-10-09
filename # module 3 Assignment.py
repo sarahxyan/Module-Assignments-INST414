be58f333e49deebd856924ca45b36c403cde182a
Sarah Yan <Sarbear9219@gmail.com>
2024-10-09 18:16:50 -0400

    Update # module 3 Assignment.py
    normalize and merge data

--- # module 3 Assignment.py
+++ # module 3 Assignment.py
@@ -6,6 +6,7 @@ import requests
 import matplotlib.pyplot as plt
 import networkx as nx
 
+
 url = "https://data.cityofchicago.org/resource/ijzp-q8t2.json"
 
 params = {
@@ -22,3 +23,12 @@ dv_data.dropna(subset=['community_area'], inplace=True)
 socioeconomic_data = pd.read_csv('socioeconomic_data.csv')
 socioeconomic_data['community_area'] = pd.to_numeric(socioeconomic_data['community_area'], errors='coerce')
 incident_counts = dv_data.groupby('community_area').size().reset_index(name='incident_count')
+
+merged_df = pd.merge(socioeconomic_data, incident_counts, on='community_area', how='left')
+features = ['incident_count', 'income', 'unemployment_rate', 'hardship_index']
+
+def normalize_column(column):
+    return (column - column.min()) / (column.max() - column.min())
+
+for feature in features:
+    merged_df[feature] = normalize_column(merged_df[feature])
